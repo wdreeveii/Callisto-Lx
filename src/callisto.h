e387db795ece993e46fd735a9ee41f7aa8015936
Whitham D. Reeve II <thetawaves@gmail.com>
2015-12-18 05:38:41 +0000

    Initial Commit

---
+++ src/callisto.h
@@ -0,0 +1,34 @@
+#ifndef CALLISTO_CALLISTO_H
+#define CALLISTO_CALLISTO_H
+
+#include <inttypes.h>
+
+#include "util.h"
+
+#define MAX_MESSAGE 128
+
+/* firmware special characters */
+#define MESSAGE_START '$'
+#define MESSAGE_END '\r'
+#define DATA_START '2'
+#define DATA_END '&'
+#define EEPROM_READY ']'
+
+extern int debug;
+
+typedef struct {
+    uint8_t *data;
+    volatile int size;
+    usec_t timestamp;  /* timestamp of the first sample */
+} buffer_t;
+
+extern int buffer_size;
+extern buffer_t buffer[2];
+extern volatile int save_buffer;
+extern volatile int active_buffer;
+
+extern volatile int start_command, overview_command, stop_command;
+
+void terminate(int signum);
+
+#endif
